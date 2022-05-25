e928ff4e05fe466d7d330f4eab5508c7fd047df8
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-05-24 23:05:17 -0500

    task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -3,20 +3,24 @@
 
 
 class Square:
-    """ """
+    """ Make a square """
 
     def __init__(self, size=0):
+        """private square"""
         self.__size = size
 
     def area(self):
+        """public square"""
         return(self.__size * self.__size)
 
     @property
     def size(self):
+        """return size square"""
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        """size square attribute"""
         try:
             ("{:d}".format(value))
             self.__size = value
