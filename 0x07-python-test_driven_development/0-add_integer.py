07f5622f5d48f5050055041aa4042d4c54462986
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-05-30 16:43:37 -0500

    first commit

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if type(a) != int and type(a) != float:
+        raise TypeError("a must be an integer")
+    elif type(b) != int and type(b) != float:
+        raise TypeError("b must be an integer")
+    else:
+        c = a + b
+        if type(c) is float:
+            c = int(c)
+        return c
