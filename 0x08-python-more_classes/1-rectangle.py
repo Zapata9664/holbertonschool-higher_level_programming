5cb6c16ff26159ff64f24a7bea77c58e10d7828c
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-06-01 03:32:22 -0500

    task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,11 +6,9 @@ class Rectangle:
     """clase rectangulo con base y altura"""
 
     def __init__(self, width=0, height=0):
-        """
-        Instancia de rectangulo 
+        """Instancia de rectangulo
         width - altura del rectangulo
-        height - base del rectangulo
-        """
+        height - base del rectangulo"""
         self.width = width
         self.height = height
 
