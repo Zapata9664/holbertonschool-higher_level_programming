42b486fff547e0982dfa0f033a3e03d91a9113ec
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-05-31 20:04:50 -0500

    task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Module for import 1-rectangle"""
+
+
+class Rectangle:
+    """
+    clase rectangulo con base y altura
+
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Instancia de rectangulo 
+        width - altura del rectangulo
+        height - base del rectangulo
+
+        """
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        """
+        Obtiene la base del rectangulo
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Establecer la base del rectangulo
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+
+    @property
+    def width(self):
+        """
+        obtiene la altura del rectangulo
+        """
+        return self.___width
+
+    @width.setter
+    def width(self, value):
+        """
+        establecer ancho del rectangulo
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    
