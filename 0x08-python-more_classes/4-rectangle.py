66d11787e74c741d6dd11c09ee212e15c842feee
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-06-01 02:36:28 -0500

    task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -3,14 +3,10 @@
 
 
 class Rectangle:
-    """clase rectangulo con base y altura"""
+    """Rectangle class defined by width and height."""
 
     def __init__(self, width=0, height=0):
-        """ 
-        Instancia de rectangulo 
-        width - altura del rectangulo
-        height - base del rectangulo
-        """
+        """clase rectangulo con base y altura"""
         self.width = width
         self.height = height
 
@@ -25,9 +21,9 @@ class Rectangle:
             rec_str += '\n'
         return rec_str[:-1]
 
-        def __repr__(self):
-            """Dibujar una instancia de rectangulo"""
-            return "Rectangle({}, {})".format(self.__width, self.__height)
+    def __repr__(self):
+        """Dibujar una instancia de rectangulo"""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
 
     @property
     def width(self):
@@ -37,7 +33,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """establecer altura del rectangulo"""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -51,7 +47,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """ Establecer base del rectangulo"""
-        if type(value) is not int:
+
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -65,4 +62,4 @@ class Rectangle:
         """ Retornar perimetro del rectangulo """
         if self.__height == 0 or self.__width == 0:
             return 0
-        return (self.__width * 2 + self.__height * 2)
+        return 2 * (self.__width + self.__height)
