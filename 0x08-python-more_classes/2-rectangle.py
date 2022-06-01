1ee3fbad75553914228044cc943cf3065faa7b41
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-05-31 21:14:42 -0500

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -48,7 +48,7 @@ class Rectangle:
         """
         Establecer la base del rectangulo
         """
-        if value is not int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
