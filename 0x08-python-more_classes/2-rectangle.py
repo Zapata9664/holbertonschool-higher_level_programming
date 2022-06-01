d8e2dc0b8c552522a6de97caa0fc96e7570c0925
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-05-31 21:11:53 -0500

    task 2

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
         height - base del rectangulo
 
         """
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -30,7 +30,7 @@ class Rectangle:
         """
         establecer ancho del rectangulo
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -48,7 +48,7 @@ class Rectangle:
         """
         Establecer la base del rectangulo
         """
-        if not isinstance(value, int):
+        if value is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -58,7 +58,7 @@ class Rectangle:
         """
         retornar el area de un rectangulo
         """
-        return self.__height * self.__width
+        return self.__width * self.__height
 
     def perimeter(self):
         """
