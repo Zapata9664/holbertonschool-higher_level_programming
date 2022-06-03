13e65f3cfec7b8484c826007d0965ba201bf6782
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-06-02 19:35:34 -0500

    try 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """ module for import text indentation"""
 
-import string
-
 
 def text_indentation(text):
     """ split strings
-    Text: text to split
-    """
+    Text: text to split"""
 
     if type(text) is not str:
         raise TypeError("text must be a string")
 
     for symbol in ".:?":
-        text = (symbol + "\n\n").join([line.strip("")
-                                       for line in text.split(symbol)])
-
+        text = (symbol + "\n\n").join(
+            [line.strip("") for line in text.split(symbol)])
     print("{}".format(text), end="")
