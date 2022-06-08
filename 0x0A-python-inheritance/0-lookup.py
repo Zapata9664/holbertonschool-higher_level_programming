2ff68663dde60996f89e6b14f2ec59193cacf159
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-06-07 19:20:43 -0500

    first commit

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Modulo for lookup
-function that returns the list of available attributes and methods of an object"""
+function that returns the list of available
+ttributes and methods of an object"""
 
 
 def lookup(obj):
