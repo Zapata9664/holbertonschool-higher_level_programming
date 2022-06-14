fc6ecfb38325e3db20479ce2d535e6deeb86c229
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-06-14 18:24:28 -0500

    test

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -17,8 +17,34 @@ class TestBase(unittest.TestCase):
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id, b2.id - 1)
+
+    def test_bool(self):
+        self.assertEqual(True, Base(True).id)
+
+    def test_NaN(self):
+        self.assertNotEqual(float('nan'), Base(float('nan')).id)
+
+
+    def test_list_id(self):
+        self.assertEqual([20, 3, 5], Base([20, 3, 5]).id)
+
+    def test_range_id(self):
+        self.assertEqual(range(5), Base(range(5)).id)
+
+    def test_bytearray_id(self):
+        self.assertEqual(bytearray(b'Hidalgo'), Base(bytearray(b'Hidalgo')).id)
+
+    def test_inf_id(self):
+        self.assertEqual(float('inf'), Base(float('inf')).id)
+
+
+
+
     
- 
+
+    
+
+
 
 
 if __name__ == "__main__":
