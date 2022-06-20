75ae84083bdc1bec117f35d47a4c15f80f0e66ed
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-06-20 13:03:11 -0500

    advanced

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+""""Write a program that prints the ASCII alphabet
+in reverse order, alternating lowercase and uppercase"""
 
-""""Write a program that prints the ASCII alphabet, in reverse order, alternating lowercase and uppercase"""
 i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
     print("{}".format(chr(c - i)), end="")
