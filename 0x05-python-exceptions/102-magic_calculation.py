ab8d88565f5447ad8c008fb15f225a1483489193
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-06-20 15:54:40 -0500

    advanced

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for magic calculation"""
+
+
+def magic_calculation(a, b):
+    """does exactly the same of a Python bytecode for Holberton"""
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except:
+            result = b + a
+            break
+    return (result)
