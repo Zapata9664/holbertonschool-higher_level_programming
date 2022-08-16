793fb9a080079dbb92b0f0cbd849652b8a949a29
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-15 22:20:15 -0500

    Picture square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
-if (process.argv[2] == null) {
-  console.log('Missing size');
-} else if (!isNaN(process.argv[2])) {
+if (!isNaN(process.argv[2])) {
   const conv = parseInt(process.argv[2]);
   let iteration = 0;
   const dr = 'X';
@@ -10,4 +8,6 @@ if (process.argv[2] == null) {
   for (iteration; iteration < conv; iteration++) {
     console.log(result);
   }
+} else {
+  console.log('Missing size');
 }
