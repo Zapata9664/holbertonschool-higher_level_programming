a76f4f15c412a543384af0963c7bd4184f96054d
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-15 21:41:32 -0500

    First loop with js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const print = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let iteration = 0;
+
+for (iteration; iteration < print.length; iteration++) {
+  console.log(print[iteration]);
+}
