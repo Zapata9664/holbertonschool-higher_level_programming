0a4e44c57e5b72d21ddc6425831d0d669307ec73
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-15 20:07:58 -0500

    Print 3 lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let PrintC, Iscool, Amazing;
+PrintC = 'C is fun';
+Iscool = 'Python is cool';
+Amazing = 'JavaScript is amazing';
+console.log(PrintC);
+console.log(Iscool);
+console.log(Amazing);
