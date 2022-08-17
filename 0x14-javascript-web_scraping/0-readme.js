7ddff9f5bed66a5e3c88f2cae6f76ca6ae7f4b70
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-17 11:16:31 -0500

    First commit

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const path = (process.argv[2]);
+const fs = require('fs');
+fs.readFile(path, 'utf8', function (err, data) {
+  if (err) {
+    return (console.log(err));
+  }
+  console.log(data);
+});
