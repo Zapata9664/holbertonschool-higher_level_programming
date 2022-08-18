e47a062d85069f6e556d281512ada4915b2e5218
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-17 19:37:45 -0500

    Actor in pelis

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,2 +1,22 @@
 #!/usr/bin/node
 
+const count= require('console');
+const request = require('request')
+const path = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(url, function(error, response, body) {
+  if (err) {
+    console.err(err);
+  }
+
+  const url = JSON.parse(body).results;
+  const iterator = 0;
+  const iteradorDos = 0;
+  for (iterator; iterator < path.length; iterator++) {
+    for (iteradorDos; iteradorDos < url[iterator].characters.length; iteradorDos++) {
+        if (path[iterator].characters[iteradorDos].endsWith('18/')) {
+            count++;
+        }
+    }
+  }
+  console.log(count);
+});
