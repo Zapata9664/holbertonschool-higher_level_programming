489974aa8b960f320c24bd58ff1446b47a3cf44e
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-17 20:13:27 -0500

    Fix order

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
+const path = (process.argv[2]);
+const file = (process.argv[3])
 
-request.get(process.argv[2], (error, response, body) => {
+request.get(path, (error, response, body) => {
   if (error) {
     console.error('error:', error);
   }
-  fs.writeFile(process.argv[3], body, err => {
+  fs.writeFile(file, body, err => {
     if (err) {
       console.error(err);
     }
