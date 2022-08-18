676c76489f171203c0cc44907817df2fadb88c29
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-17 20:08:13 -0500

    Fix order

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,11 +5,13 @@ const path = (process.argv[2]);
 const fs = require('fs');
 const file = process.argv[3];
 
-request(path, function (err, response, body) {
-  if (err) {
-    return console.log(err);
+request.get(path, (error, response, body) => {
+  if (error) {
+    console.error('error:', error);
   }
-  fs.writeFile(file, body, 'utf8', function (err) {
-    if (err) return console.log(err);
+  fs.writeFile(file, body, err => {
+    if (err) {
+      console.error(err);
+    }
   });
 });
