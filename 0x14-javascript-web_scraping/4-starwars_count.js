d420fb1a289a348d55ddee824077378e44d82309
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-17 19:51:38 -0500

    Actor in pelis

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+
+const require = require('axios').default;
+const path = (require.get(process.argv[2]))
+path.then(function (response) {
+  let count = 0;
+  for (const pelis of response.data.results) {
+    for(const actor of list.characters) {
+      if (actor.endsWith('18/')) {
+        count++;
+        break;
+    }
+  }
+}
+  console.log(count);
+})
+
+.catch(function(err) {
+  console.log('code :' + error.response.status);
+});
