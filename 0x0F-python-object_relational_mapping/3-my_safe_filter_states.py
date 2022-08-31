a2cb78cb4c3862f118d96fe52d72f2d8c34add60
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-08-31 16:25:14 -0500

    Save filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 '''Lists all states from the database hbtn_0e_0_usa'''
-import sys
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306,
                            user=sys.argv[1], passwd=sys.argv[2],
                            db=sys.argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name='{:s}'\
-                ORDER BY id ASC".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                (sys.argv[4],))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
