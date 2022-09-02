2f0276400e27ef948019198de8df327f73438e60
Zapata9664 <valentinazapatajaramillo@gmail.com>
2022-09-02 11:56:10 -0500

    Display methods

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-#  takes in a URL and displays all HTTP methods the server will accept
+# Displays all HTTP methods the server will accept
 curl -sI "$1" | grep "Allow" | cut -d " " -f2
